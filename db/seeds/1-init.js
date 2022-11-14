c782e5ac6f4b0b46a582719664ada705d2ca282f
Ryota Nakamura <snaryon@gmail.com>
2022-11-14 09:42:16 +0900

    modified the seed.

--- db/seeds/1-init.js
+++ db/seeds/1-init.js
@@ -7,6 +7,7 @@
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("dict").del();
+  await knex.raw("TRUNCATE TABLE dict RESTART IDENTITY CASCADE");
   await knex("dict").insert([
     { ja: "名探偵コナン", en: "Case Closed" }, // 日本語・英語に別な文字列が設定
     { ja: "Slam dunk", en: "Slam dunk" }, // 日本語・英語に同じ文字列が設定
